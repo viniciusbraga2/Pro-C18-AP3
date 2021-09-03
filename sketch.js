09c1b971be7b78ec599000c24373adfb3b29547f
viniciusbraga2 <87631011+viniciusbraga2@users.noreply.github.com>
2021-09-02 22:51:32 -0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,22 +1,22 @@
 function setup() {
   createCanvas(400, 400);
   
-  //A string data-type
+  //Um string do tipo de dados
   name = "Hermoine";
   
-  //A number data-type
+  //Um número do tipo de dados
   num = 31;
   
-  //An array data-type
+  //Uma matriz do tipo de dados
   newArray = ["Item1", 32, name, num];
   
-  //accessing the 1st item in the array and printing it on the console
+  //acessando o 1º item na matriz e colocando-o no console
   console.log(newArray[0]);
   
-  //accessing the 3rd item in the array and printing it on the console
+  //acessando o 3º item na matriz e colocando-o no console
   console.log(newArray[2]);
 
-  //finding the length of the array
+  //encontrando o comprimento da matriz
   console.log(newArray.length);
 
 }
